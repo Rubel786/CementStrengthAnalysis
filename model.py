bfb5bbf80b62dcccd36ca4ec2679de3b727978c0
Rubel Shaikh <44048879+Rubel786@users.noreply.github.com>
2023-11-02 20:45:23 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,10 +1,7 @@
 # Importing the libraries
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
+import pandas as pd
 import pickle
 
 df = pd.read_csv('Cleaned_MCS.csv')
@@ -12,33 +9,38 @@ df = pd.read_csv('Cleaned_MCS.csv')
 X = df.drop('Compression_Strength', axis=1)
 y = df['Compression_Strength']
 
+#Splitting Training and Test Set
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
 
-# Standardize features
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
-
-# Random Forest Regressor
-model_rf = RandomForestRegressor(random_state=50)
-
-#Fitting model with trainig data
-param_grid_rf = {
-    'n_estimators': [10, 50, 100],
-    'max_depth': [None, 10, 20],
+param_grid = {
+    'n_estimators': [50, 100, 150],  # You can adjust the number of trees
+    'max_depth': [None, 10, 20, 30],  # You can adjust the maximum depth of trees
+    'min_samples_split': [2, 5, 10],  # You can adjust the minimum samples required to split a node
+    'min_samples_leaf': [1, 2, 4],  # You can adjust the minimum samples required for a leaf node
 }
-grid_search_rf = GridSearchCV(model_rf, param_grid_rf, cv=5)
-grid_search_rf.fit(X_train, y_train)
-best_rf = grid_search_rf.best_estimator_
 
-best_rf.fit(X_train, y_train)
+rf_model = RandomForestRegressor(random_state=42)
+
+# Create a Grid Search with cross-validation
+grid_search = GridSearchCV(rf_model, param_grid, cv=5, scoring='neg_mean_squared_error')
+
+# Fit the Grid Search to find the best hyperparameters
+grid_search.fit(X_train, y_train)
+
+best_params = grid_search.best_params_
+
+# Train the model with the best hyperparameters
+best_rf_model = RandomForestRegressor(random_state=42, **best_params)
+best_rf_model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = best_rf_model.predict(X_test)
+
 
 # Saving model to disk
-pickle.dump(best_rf, open('model.pkl','wb'))
+pickle.dump(best_rf_model, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[486.42, 180.60, 21.26, 201.66, 16.11,1151.17,708.50,344.43]]))
+print(model.predict([[2, 9, 6, 8, 9, 7, 8, 7]]))
--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Material_Quantity':2, 'Additive_Catalyst':9, 'Ash_Component':6,
+r = requests.post(url,json={'Material_Quantity':5, 'Additive_Catalyst':7, 'Ash_Component':10,
                             'Water_Mix':2, 'Plasticizer':9, 'Moderate_Aggregator':6,
                             'Refined_Aggregator':2, 'Formulation_Duration':9})
 
